aefa618095c1e381eefd4ddbd01339cfe493d36a
Pablo Rosa <pablorsantos15@gmail.com>
2025-09-15 20:35:41 -0300

    chore(Mobile): start project

---
+++ mobile/babel.config.js
@@ -0,0 +1,32 @@
+module.exports = function (api) {
+  api.cache(true);
+
+  return {
+    presets: [
+      [
+        'babel-preset-expo',
+        {
+          jsxImportSource: 'nativewind',
+        },
+      ],
+      'nativewind/babel',
+    ],
+
+    plugins: [
+      'react-compiler',
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+
+          alias: {
+            '@': './',
+            'tailwind.config': './tailwind.config.js',
+          },
+        },
+      ],
+      'react-native-worklets/plugin'
+    ],
+  };
+};
+
